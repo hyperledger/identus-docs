784e5090c1e8247c1360e6238fb448f38b32648a
Ianos <ianos.dorultan@iohk.io>
2023-03-17 13:06:22 +0200

    style: main hero

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import AtalaGraphic from '../atala-graphic';
 
 const FeatureList = [
   {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,21 +6,25 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
+import AtalaGraphic from '../components/atala-graphic';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/getting-started">
-            Get Started
-          </Link>
-        </div>
+    <header className={clsx('hero', styles.heroBanner)}>
+      <div className={clsx('container', styles.container)}>
+          <div className={styles.hero__content}>
+              <span className={clsx('hero__subtitle', styles.hero__tagline)}>{siteConfig.tagline}</span>
+              <h1 className={clsx('hero__title', styles.hero__title)} dangerouslySetInnerHTML={{__html: siteConfig.title}}/>
+              <div className={styles.buttons}>
+                  <Link
+                      className={clsx("button button--lg", styles.hero__button)}
+                      to="/docs/getting-started">
+                      Get Started
+                  </Link>
+              </div>
+          </div>
+          <AtalaGraphic/>
       </div>
     </header>
   );
