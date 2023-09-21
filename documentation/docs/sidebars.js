ae570f748327cadeb75feeee53d5f6da5e412e21
patlo-iog <pat.losoponkul@iohk.io>
2023-09-21 15:58:04 +0700

    fix: refine multi-tenancy documentation (#55)
    Signed-off-by: Pat Losoponkul <pat.losoponkul@iohk.io>
    Co-authored-by: Pete Vielhaber <95773776+petevielhaber@users.noreply.github.com>

--- documentation/docs/sidebars.js
+++ documentation/docs/sidebars.js
@@ -60,7 +60,6 @@ const sidebars = {
           items: [
             'atala-prism/prism-cloud-agent/overview',
             'atala-prism/prism-cloud-agent/building-blocks',
-            'atala-prism/prism-cloud-agent/multi-tenancy-management',
             'atala-prism/prism-cloud-agent/authentication',
             'atala-prism/prism-cloud-agent/did-management',
             'atala-prism/prism-cloud-agent/secrets-storage',
