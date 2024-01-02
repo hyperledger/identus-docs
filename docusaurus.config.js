d1c45c634d854a5e5180f172ec4153b1ff0dd927
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2024-01-02 22:54:11 +0100

    fix: build the typescript SDK from submodules on the fly.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@
 
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const path = require('path')
 // const {useColorMode} = require('@docusaurus/theme-common');
 // const {colorMode} = useColorMode()
 // console.log(colorMode)
@@ -40,7 +41,7 @@ const config = {
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
                 },
-            }),
+            })
         ],
 
         /**
@@ -56,13 +57,14 @@ const config = {
                         spec: 'atala-prism-building-blocks/prism-agent/service/api/http/prism-agent-openapi-spec.yaml',
                         route: '/agent-api/',
                     },
-                    {
-                        spec: "atala-prism-products/enterprise-services/api-spec/enterprise-services-spec.yml",
-                        route: "/enterprise-api/",
-                    }
+                    // {
+                    //     spec: "atala-prism-products/enterprise-services/api-spec/enterprise-services-spec.yml",
+                    //     route: "/enterprise-api/",
+                    // }
                 ]
             },
         ],
+
     ],
     plugins: [
         [
@@ -74,7 +76,39 @@ const config = {
                 sidebarPath: require.resolve('./atala-prism-building-blocks/docs/docusaurus/sidebars.js'),
             },
         ],
-        require.resolve('docusaurus-lunr-search')
+        require.resolve('docusaurus-lunr-search'),
+        [
+            'docusaurus-plugin-typedoc',
+                {
+                    // TypeDoc options
+                    entryPoints: ['./atala-prism-wallet-sdk-ts/src/index.ts'],
+                    tsconfig: './atala-prism-wallet-sdk-ts/tsconfig.json',
+                    "name": "@input-output-hk/atala-prism-wallet-sdk",
+                    "excludeExternals": false,
+                    "plugin": ["typedoc-plugin-rename-defaults", "typedoc-plugin-markdow"],
+                    "useTsLinkResolution": true,
+                    "hideGenerator": true,
+                    "entryPointStrategy": "resolve",
+                    "excludePrivate": false,
+                    "excludeReferences": false,
+                    "excludeProtected": false,
+                    "excludeInternal": false,
+                    "excludeNotDocumented": true,
+                    "excludeNotDocumentedKinds": [
+                        "Variable"
+                    ],
+                    // Plugin options
+                    out: path.resolve(__dirname, './atala-prism-wallet-sdk-ts/docs-generated')
+                },
+          ],
+          [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'atala-prism-wallet-sdk-ts',
+                path: 'atala-prism-wallet-sdk-ts/docs-generated',
+                routeBasePath: 'atala-prism-wallet-sdk-ts',
+            },
+        ]
     ],
 
     themes: ["docusaurus-theme-openapi-docs", '@docusaurus/theme-mermaid', "docusaurus-theme-redoc"],
@@ -89,6 +123,7 @@ const config = {
                     srcDark: "img/logo-navbar-light.png",
                 },
                 items: [
+                   
                     {
                         type: 'doc',
                         docId: 'getting-started',
@@ -123,8 +158,9 @@ const config = {
                                 href: 'https://input-output-hk.github.io/atala-prism-wallet-sdk-swift/',
                             },
                             {
-                                label: 'Wallet SDK TS',
-                                href: 'https://input-output-hk.github.io/atala-prism-wallet-sdk-ts/',
+                                to: '/atala-prism-wallet-sdk-ts/',
+                                label: 'Wallet SDK Typescript',
+                                activeBaseRegex: `/atala-prism-wallet-sdk-ts/`
                             },
                             {
                                 label: 'Wallet SDK Kotlin Multiplatform',
